a36364976af776fbbe09602fa77d167dcc5f10f3
Or Ron <or@cellutions.co.il>
2013-08-19 17:30:44 +0300

    Added more files

---
+++ DDIndicator.h
@@ -0,0 +1,18 @@
+//
+//  DDIndicator.h
+//  loading
+//
+//  Created by Or Ron on 4/6/13.
+//  Copyright (c) 2013 Or Ron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDIndicator : UIView
+
+
+-(void) startAnimating;
+-(void) stopAnimating;
+
+
+@end
